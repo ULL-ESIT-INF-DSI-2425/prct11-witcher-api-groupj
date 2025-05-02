f4f7b3623a7dd60c72de07905559bd3c669faff9
Iván Pérez <alu0101477510@ull.edu.es>
2025-05-02 18:21:13 +0000

    Index creado para probar

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import './db/mongoose.js';
+import { userRouter } from './routers/user.js';
+import { noteRouter } from './routers/note.js';
+import { defaultRouter } from './routers/default.js';
+
+const app = express();
+app.use(express.json());
+app.use(userRouter);
+app.use(noteRouter);
+app.use(defaultRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
