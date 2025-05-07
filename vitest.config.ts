fcb80f4ec6dd9365bc99b42a368af8e351238c93
Iván Pérez <alu0101477510@ull.edu.es>
2025-05-07 22:30:14 +0000

    Opciones para los tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['tests/index.spec.ts'], // Solo ejecuta este archivo
+    include: ['tests/index.spec.ts'], 
     coverage: {
-      all: true, // Fuerza medir cobertura en todos los archivos incluidos
-      reporter: ['text'], // Resultados en consola y para HTML
-      include: ['src/**/*.ts'], // Solo mide archivos de src/
+      all: true, 
+      reporter: ['text', 'lcov'],
+      include: ['src/**/*.ts'], 
       exclude: [
-        '**/*.d.ts',       // Excluye definiciones de tipo
-        'docs/**',         // Excluye docs
-        'dist/**',         // Excluye carpeta de distribución
-        'node_modules/**', // Excluye dependencias
-        'tests/**',        // Excluye tests en sí
+        '**/*.d.ts',       
+        'docs/**',         
+        'dist/**',         
+        'node_modules/**', 
+        'tests/**',       
       ],
     },
   },
